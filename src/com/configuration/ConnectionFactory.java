21ec4781fb1ff4869ac2d5a884d6f333f1ba1cbe
Shri <Shri@DESKTOP-HQS310M>
2021-09-21 12:15:23 +0530

    some other file of project

---
+++ src/com/configuration/ConnectionFactory.java
@@ -0,0 +1,18 @@
+package com.configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory 
+{
+	public static final String JDBC_URL="jdbc:mysql://localhost:3306/appraisal_system";
+	public static final String USER="root";
+	public static final String PASS="";
+	public static Connection getConnection()throws SQLException
+	{
+		Connection connection=null;
+		connection=DriverManager.getConnection(JDBC_URL,USER,PASS);
+		return connection;
+	}		
+}
